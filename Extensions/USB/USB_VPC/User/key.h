b1a98b24a6aa566ae14e9d960d3673498ac4db3f
Ld242493 <Ld242493@VirtualEmail.com>
2024-08-24 23:25:00 +0800

    first commit

---
+++ Extensions/USB/USB_VPC/User/key.h
@@ -0,0 +1,19 @@
+#ifndef __KEY_H
+#define __KEY_H
+
+#include "main.h"
+
+typedef enum {
+  Key_None  = 0,
+  Key_Enter,
+  Key_Up,
+  Key_Down,
+  Key_Enter_long
+}key_type;
+
+
+extern key_type Key_Val;
+
+void key_init(void);
+
+#endif
